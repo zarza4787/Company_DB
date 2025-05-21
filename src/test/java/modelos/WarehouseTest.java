69bcdade40892184f32dd5b4355f04e1087bf1cd
Alvaro <azarbea179@g.educaand.es>
2025-05-21 20:00:52 +0200

    Update WarehouseTest.java
    Implementado Hamcrest

--- src/test/java/modelos/WarehouseTest.java
+++ src/test/java/modelos/WarehouseTest.java
@@ -1,56 +1,39 @@
 package modelos;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 import excepciones.DatoIncorrrectoException;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class WarehouseTest {
 
-	@Test
-	public void testConstructorWarehouse() throws DatoIncorrrectoException {
-		Warehouse wh = new Warehouse(1, 10, "Almacen Central");
-		assertEquals(1, wh.getWareHouseId());
-		assertEquals(10, wh.getLocationID());
-		assertEquals("Almacen Central", wh.getNameWareHouse());
-	}
+    @Test
+    public void testWarehouseLongLongString() throws DatoIncorrrectoException {
+        Warehouse wh = new Warehouse(1, 10, "Almacen Sevilla");
+        assertThat(wh.getWareHouseId(), is(1L));
+        assertThat(wh.getLocationID(), is(10L));
+        assertThat(wh.getNameWareHouse(), is("Almacen Sevilla"));
+    }
 
-	@Test
-	public void testConstructorWarehouseActualizar() throws DatoIncorrrectoException {
-		Warehouse wh = new Warehouse(10, "Almacen Secundario");
-		assertEquals(10, wh.getLocationID());
-		assertEquals("Almacen Secundario", wh.getNameWareHouse());
-	}
+    @Test
+    public void testWarehouseLongString() throws DatoIncorrrectoException {
+        Warehouse wh = new Warehouse(10, "Almacen Huelva");
+        assertThat(wh.getLocationID(), is(10L));
+        assertThat(wh.getNameWareHouse(), is("Almacen Huelva"));
+        assertThat(wh.getWareHouseId(), is(0L)); 
+    }
 
-	@Test
-	public void testConstructorWarehouseEliminar() throws DatoIncorrrectoException {
-		Warehouse wh = new Warehouse(5);
-		assertEquals(5, wh.getWareHouseId());
-	}
+    @Test
+    public void testWarehouseLong() throws DatoIncorrrectoException {
+        Warehouse wh = new Warehouse(5);
+        assertThat(wh.getWareHouseId(), is(5L));
+    }
 
-	@Test
-	public void testGetWareHouseId() throws DatoIncorrrectoException {
-		Warehouse wh = new Warehouse(2, 20, "Almacen");
-		assertEquals(2, wh.getWareHouseId());
-	}
+    @Test
+    public void testToString() throws DatoIncorrrectoException {
+        Warehouse wh = new Warehouse(2, 20, "Almacen");
+        assertThat(wh.toString(), is("Almacen [WAREHOUSE_ID=2, LOCATION_ID=20, WAREHOUSE_NAME=Almacen]"));
+    }
 
-	@Test
-	public void testGetLocationID() throws DatoIncorrrectoException {
-		Warehouse wh = new Warehouse(2, 20, "Almacen");
-		assertEquals(20, wh.getLocationID());
-	}
-
-	@Test
-	public void testGetNameWareHouse() throws DatoIncorrrectoException {
-		Warehouse wh = new Warehouse(2, 20, "Almacen");
-		assertEquals("Almacen", wh.getNameWareHouse());
-	}
-
-	@Test
-	public void testToString() throws DatoIncorrrectoException {
-		Warehouse wh = new Warehouse(2, 20, "Almacen");
-		String expected = "Almacen [WAREHOUSE_ID=2, LOCATION_ID=20, WAREHOUSE_NAME=Almacen]";
-		assertEquals(expected, wh.toString());
-	}
+   
 }
