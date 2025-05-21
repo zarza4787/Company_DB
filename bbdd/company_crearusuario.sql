6cd1dfdb18709e72520edd70c8a2e4e5039de8d4
Alvaro <azarbea179@g.educaand.es>
2025-05-21 12:48:38 +0200

    Nuevos cambios

--- bbdd/company_crearusuario.sql
+++ bbdd/company_crearusuario.sql
@@ -2,3 +2,6 @@
 CREATE USER company_db IDENTIFIED BY company;
 
 GRANT CONNECT, RESOURCE, DBA TO company_db;
+
+
+
--- bbdd/company_drop.sql
+++ bbdd/company_drop.sql
@@ -1,12 +1,27 @@
-DROP TABLE company_db.order_items;  
-DROP TABLE company_db.orders;
-DROP TABLE company_db.inventories;
-DROP TABLE company_db.products;
-DROP TABLE company_db.product_categories;
-DROP TABLE company_db.warehouses;
-DROP TABLE company_db.employees;
-DROP TABLE company_db.contacts;
-DROP TABLE company_db.customers;
-DROP TABLE company_db.locations;
-DROP TABLE company_db.countries;
-DROP TABLE company_db.regions;
+DROP TABLE C##COMPANYzarza.order_items;  
+DROP TABLE C##COMPANYzarza.orders;
+DROP TABLE C##COMPANYzarza.inventories;
+DROP TABLE C##COMPANYzarza.products;
+DROP TABLE C##COMPANYzarza.product_categories;
+DROP TABLE C##COMPANYzarza.warehouses;
+DROP TABLE C##COMPANYzarza.employees;
+DROP TABLE C##COMPANYzarza.contacts;
+DROP TABLE C##COMPANYzarza.customers;
+DROP TABLE C##COMPANYzarza.locations;
+DROP TABLE C##COMPANYzarza.countries;
+DROP TABLE C##COMPANYzarza.regions;
+
+
+DROP TABLE C##COMPANYzarza.order_items;  
+DROP TABLE C##COMPANYzarza.orders;
+DROP TABLE C##COMPANYzarza.inventories;
+DROP TABLE C##COMPANYzarza.products;
+DROP TABLE C##COMPANYzarza.product_categories;
+DROP TABLE C##COMPANYzarza.warehouses;
+DROP TABLE C##COMPANYzarza.employees;
+DROP TABLE C##COMPANYzarza.contacts;
+DROP TABLE C##COMPANYzarza.customers;
+DROP TABLE C##COMPANYzarza.locations;
+DROP TABLE C##COMPANYzarza.countries;
+DROP TABLE C##COMPANYzarza.regions;
+
